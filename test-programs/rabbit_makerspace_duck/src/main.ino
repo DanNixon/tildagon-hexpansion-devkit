41a023aa1aa636d766fa7d15ff84380134f156dc
Dan Nixon <dan@dan-nixon.com>
2024-05-14 18:14:48 +0100

    More Maker Space M testing

--- test-programs/rabbit_makerspace_duck/src/main.ino
+++ test-programs/rabbit_makerspace_duck/src/main.ino
@@ -68,13 +68,72 @@ void setup() {
 
   select_sys_i2c();
 
-  badge_io.analogWrite(rabbit_red_pin, 8);
-  badge_io.analogWrite(duck_blue_pin, 8);
+  badge_io.analogWrite(rabbit_red_pin, 4);
+  badge_io.analogWrite(duck_blue_pin, 4);
 
   select_port_e_i2c();
 }
 
 void loop() {
+  set_m_intensity(0);
+  m_breathe_effect();
+
+  set_m_intensity(0);
+  m_scroll_effect();
+}
+
+// clang-format off
+std::array<std::tuple<int, int>, 29> segments = {
+    std::make_tuple(0, 7),
+    std::make_tuple(0, 12),
+    std::make_tuple(0, 13),
+    std::make_tuple(0, 14),
+    std::make_tuple(0, 15),
+    std::make_tuple(0, 6),
+    std::make_tuple(0, 5),
+    std::make_tuple(0, 4),
+    std::make_tuple(0, 3),
+    std::make_tuple(0, 2),
+    std::make_tuple(0, 1),
+    std::make_tuple(0, 0),
+    std::make_tuple(0, 11),
+    std::make_tuple(0, 10),
+    std::make_tuple(0, 9),
+    std::make_tuple(0, 8),
+    std::make_tuple(1, 7),
+    std::make_tuple(1, 12),
+    std::make_tuple(1, 6),
+    std::make_tuple(1, 5),
+    std::make_tuple(1, 4),
+    std::make_tuple(1, 3),
+    std::make_tuple(1, 2),
+    std::make_tuple(1, 1),
+    std::make_tuple(1, 0),
+    std::make_tuple(1, 8),
+    std::make_tuple(1, 9),
+    std::make_tuple(1, 10),
+    std::make_tuple(1, 11),
+};
+// clang-format on
+
+void m_scroll_effect() {
+  for (const auto &segment : segments) {
+    auto expander = std::get<0>(segment);
+    auto pin = std::get<1>(segment);
+
+    set_m_intensity(0);
+    if (expander == 0) {
+      ms_hexpansion_io_1.analogWrite(pin, 12);
+    } else if (expander == 1) {
+      ms_hexpansion_io_2.analogWrite(pin, 12);
+    }
+
+    Serial.println("-");
+    delay(50);
+  }
+}
+
+void m_breathe_effect() {
   for (int i = 0; i < 24; i++) {
     set_m_intensity(i);
     delay(10);
